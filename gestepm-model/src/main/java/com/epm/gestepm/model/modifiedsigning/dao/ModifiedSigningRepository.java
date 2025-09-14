b4c6c2ea7695693f70207f8f6a4c6feef35e5d7d
Argenis Rodriguez <argenismrvel@gmail.com>
2025-09-14 02:11:30 -0400

    [remove] modify signing view, repository, service, dto and table

--- gestepm-model/src/main/java/com/epm/gestepm/model/modifiedsigning/dao/ModifiedSigningRepository.java
+++
@@ -1,8 +0,0 @@
-package com.epm.gestepm.model.modifiedsigning.dao;
-
-import com.epm.gestepm.modelapi.modifiedsigning.dto.ModifiedSigning;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ModifiedSigningRepository extends CrudRepository<ModifiedSigning, Long>, ModifiedSigningRepositoryCustom {
-
-}
